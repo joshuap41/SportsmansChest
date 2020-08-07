149a49c8b9f3c720e5ab7bbd1d7896ea56f2c382
joshuap41 <joshua.j@outlook.com>
2020-08-06 21:58:50 -0400

    backup

---
+++ SportsmansChest/View/PastEventPages/ProfilePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace SportsmansChest.View.PastEventPages
+{
+    public partial class ProfilePage : ContentPage
+    {
+        public ProfilePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
