8d6c4b5290e414feba0e84a7b90faffc7d74f083
Joshua Johnson <joshua.j@outlook.com>
2020-08-14 20:35:21 -0400

    unit test is working now

---
+++ SportsmansChest.MethodTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SportsmansChest.MethodTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SportsmansChest.MethodTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a9bd7664-390a-461b-a04f-98baacf8ab00")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SportsmansChest.MethodTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SportsmansChest.Model;
+
+namespace SportsmansChest.MethodTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange - Arrange part I have created an instance of Method Class which I have written in PCL project
+            InventoryItem inventoryItem = new InventoryItem();
+            int expectedResult = 16;
+
+            // Act - ACT in this portion I am passing the value in method parameter.
+            int actualResult = inventoryItem.Addititon(11, 5);
+
+            // Assert - Assert in this part you can see I am comparing values.
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
