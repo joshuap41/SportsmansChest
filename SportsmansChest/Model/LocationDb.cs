40bfa5230d1fee8a648031b1c846093f097de1df
joshua.j <joshua.j@outlook.com>
2020-08-18 10:48:15 -0400

    backup

---
+++ SportsmansChest/Model/LocationDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SQLite;
+namespace SportsmansChest.Model
+{
+    public class LocationDb
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string LocationName { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string EventType { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public string Notification { get; set; }
+        public string Notes { get; set; }
+        //pictures
+
+    }
+}
