7d85ce2e3ae30d94df4ce9662c0e909ec4f7109d
joshua.j <joshua.j@outlook.com>
2020-10-22 08:42:23 -0400

    App icons for both stores are now done.

--- SportsmansChest.Android/MainActivity.cs
+++ SportsmansChest.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using System.IO;
 
 namespace SportsmansChest.Droid
 {
-    [Activity(Label = "SportsmansChest", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SportsmansChest", Icon = "@drawable/playstore", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
